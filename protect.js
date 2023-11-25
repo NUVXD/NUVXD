1d7aba3c5d5b02b77464900b8c5b5bb4889d94e2
Nuv <150278355+NUVXD@users.noreply.github.com>
2023-11-25 23:37:29 +0100

    protecto

--- protect.js
+++ protect.js
@@ -1,21 +1,14 @@
-// protect.js
-
-const secretCode = process.env.CODE; // Retrieve the secret code from GitHub Secrets
+const secretCode = process.env.CODE;
 
 function checkPassword() {
   const userInput = document.getElementById('secretCode').value;
-
   if (userInput === secretCode) {
-    // Correct code, redirect to secret page
-    window.location.href = 'main.html'; // Update 'secret-page.html' with your actual secret page URL
+    window.location.href = 'Main.html';
   } else {
-    // Incorrect code, stay on the same page
-    alert('Access denied!'); // You can customize this message or add your logic here
+    alert('Access denied!');
   }
 }
 
-// Check if the user manually visits the secret page
-if (window.location.href.includes('main.html')) {
-  // Redirect back to the access page
-  window.location.href = 'index.html'; // Update 'index.html' with your actual access page URL
+if (window.location.pathname.includes('Main.html') && userInput !== secretCode) {
+  window.location.href = 'Index.html';
 }
