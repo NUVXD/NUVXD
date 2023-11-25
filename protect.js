de41fab6d4093532d6c2ab1b6017c245dd5d3e3c
Nuv <150278355+NUVXD@users.noreply.github.com>
2023-11-25 23:22:53 +0100

    protect.js

--- protect.js
+++ protect.js
@@ -6,10 +6,16 @@ function checkPassword() {
   const userInput = document.getElementById('secretCode').value;
 
   if (userInput === secretCode) {
-    // Correct code, allow access or perform necessary actions
-    alert('Access granted!'); // You can customize this message or add your logic here
+    // Correct code, redirect to secret page
+    window.location.href = 'main.html'; // Update 'secret-page.html' with your actual secret page URL
   } else {
-    // Incorrect code, deny access or handle accordingly
+    // Incorrect code, stay on the same page
     alert('Access denied!'); // You can customize this message or add your logic here
   }
+}
+
+// Check if the user manually visits the secret page
+if (window.location.href.includes('main.html')) {
+  // Redirect back to the access page
+  window.location.href = 'index.html'; // Update 'index.html' with your actual access page URL
 }
