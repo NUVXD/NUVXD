6507bfc29951ae389a062715facd954b7d4007b7
Nuv <150278355+NUVXD@users.noreply.github.com>
2023-11-26 12:56:34 +0100

    d

--- protect.js
+++ protect.js
@@ -1,32 +1,24 @@
 // protect.js
 
-const secretCode = process.env.CODE;
-
 document.addEventListener('DOMContentLoaded', function () {
   const submitButton = document.getElementById('submitButton');
   submitButton.addEventListener('click', checkPassword);
 });
 
 function checkPassword() {
-  console.log('Function called');
-  const userInput = document.getElementById('secretCode').value.trim(); // Trim whitespace
-  console.log('User input:', userInput);
-  console.log('Secret code:', secretCode);
+  const userInput = document.getElementById('secretCode').value.trim();
+  const githubSecret = window.GITHUB_SECRET || 'fallback-secret'; // Use a fallback or handle missing secret gracefully
 
-  if (userInput === secretCode) {
-    console.log('Redirecting to Main.html');
+  if (userInput === githubSecret) {
     window.location.href = 'Main.html';
   } else {
-    console.log('Access denied!');
     alert('Access denied!');
   }
 }
 
 // Check if the user manually visits the secret page
-if (window.location.pathname.includes('Main.html') && secretCode === undefined) {
-  console.log('Manual visit to secret page. Redirecting to Index.html');
+if (window.location.pathname.includes('Main.html') && window.GITHUB_SECRET === undefined) {
   window.location.href = 'Index.html';
-} else if (window.location.pathname.includes('Index.html') && secretCode !== undefined) {
-  console.log('Manual visit to access page. Redirecting to Main.html');
+} else if (window.location.pathname.includes('Index.html') && window.GITHUB_SECRET !== undefined) {
   window.location.href = 'Main.html';
 }
