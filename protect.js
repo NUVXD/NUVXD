756e21ed659849af778896bbb2bb8974579f7f7a
Nuv <150278355+NUVXD@users.noreply.github.com>
2023-11-26 14:09:35 +0100

    s

--- protect.js
+++ protect.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const submitButton = document.getElementById('submitButton');
+  const passwordForm = document.getElementById('passwordForm');
 
-  if (submitButton) {
-    console.log('Submit button found. Adding event listener.');
-    submitButton.addEventListener('click', function () {
-      console.log('Button clicked. Calling checkPassword.');
+  if (passwordForm) {
+    passwordForm.addEventListener('submit', function (event) {
+      // Prevent the default form submission behavior
+      event.preventDefault();
+      
+      console.log('Form submitted. Calling checkPassword.');
       checkPassword();
     });
   } else {
-    console.error('Error: Submit button not found');
+    console.error('Error: Password form not found');
   }
 });
 
